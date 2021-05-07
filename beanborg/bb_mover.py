1e8858a718a7df4b53bde3931f91a539c7eb26c5
Luciano Fiandesio <luciano@fiandes.io>
2021-05-07 12:02:56 +0200

    Add `post_move_script` to post-process file (#5)
    * Add `post_move_script` to post-process file
    
    `post_move_script` allow to specify a path to a script that
    is executed after the file is moved to the target directory.

--- beanborg/bb_mover.py
+++ beanborg/bb_mover.py
@@ -7,16 +7,17 @@ __license__ = "GNU GPLv2"
 import glob
 import os
 import sys
+from subprocess import check_call, CalledProcessError
 
 from beanborg.arg_parser import eval_args
 from beanborg.config import init_config
-
 
 def main():
     
     args = eval_args('Move bank csv file to processing folder')
     config = init_config(args.file, args.debug)
-    
+    current_dir = os.getcwd()
+
     if not os.path.isdir(config.csv.download_path):
         print("folder: %s does not exist!"%(config.csv.download_path))
         sys.exit(-1)
@@ -38,8 +39,13 @@ def main():
 
     for f in os.listdir(config.csv.download_path):
         if f.startswith(config.csv.name):
-            os.rename(config.csv.download_path + "/" + f, config.csv.target + "/" + config.csv.ref + ".csv")
-
+            moved_csv = os.path.join(config.csv.target, config.csv.ref + ".csv")
+            os.rename(config.csv.download_path + "/" + f, moved_csv)
+            if(config.csv.post_script_path):
+                try:
+                    check_call([config.csv.post_script_path, os.path.join(current_dir, moved_csv)])
+                except CalledProcessError as e:
+                    print("An error occurred executing: %s\n%s"%(config.csv.post_script_path, str(e)))
     print("Done :) ")
 
 
