9c03150daa7346928a1b60873acfda9906834251
Luciano Fiandesio <luciano@fiandes.io>
2021-11-23 12:38:27 +0100

    code formatting

---
+++ beanborg/utils/duplicate_detector.py
@@ -0,0 +1,46 @@
+import hashlib
+from beancount import loader
+from beancount.core.data import Transaction
+from beanborg.utils.input import query_yes_no
+
+
+def hash_tuple(tuple):
+
+    m = hashlib.md5()
+    for s in tuple:
+        m.update(str(s).encode("utf-8"))
+    return m.hexdigest()
+
+
+def to_tuple(transaction):
+
+    return (str(transaction.date), transaction.payee, transaction.postings[0].units)
+
+
+def init_duplication_store(account, journal):
+    """
+    Builds a map of existing transactions for the account being imported.
+    Each map entry has an hash of the value as key and a tuple of transaction date,
+    amount and payee as value.
+    This map is used to report identical transactions being imported,
+    should the standard hash based approach fail.
+    """
+    transactions = {}
+    entries, _, _ = loader.load_file(journal)
+    for entry in entries:
+        if isinstance(entry, Transaction):
+            if entry.meta["filename"].endswith(account):
+                tup = to_tuple(entry)
+                transactions[hash_tuple(tup)] = tup
+
+    return transactions
+
+
+def print_duplication_warning(tx):
+
+    print(
+        "Warning: a transaction with identical information already exists.\
+        \ndate: %s\npayee: %s\namount %s"
+        % (tx[0], tx[1], tx[2])
+    )
+    return query_yes_no("Do you want to import it?")
