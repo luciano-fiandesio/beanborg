fa03a6b851c2f4253ee9a17067577c2bd3ab3744
Luciano Fiandesio <luciano@fiandesio.com>
2023-10-03 12:55:47 +0200

    Fix setup issue with outdated dependencies

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
-import setuptools
+from setuptools import setup, find_packages
 
-# just a minimal setup.py file for backwards compatibility. Actual config is done in setup.cfg.
-setuptools.setup()
+# Read requirements.txt
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+setup(
+    name='beanborg',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=required,
+    entry_points={
+        'console_scripts': [
+        	'bb_mover=beanborg:bb_mover.main',
+        	'bb_archive=beanborg:bb_archive.main',
+        	'bb_import=beanborg:bb_import.main'
+        ],
+    }
+)
