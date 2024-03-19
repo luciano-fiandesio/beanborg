138698824c1d51158ecd17a4da01eb9114d15306
Luciano Fiandesio <luciano@fiandesio.com>
2024-03-19 21:32:02 +0100

    Fix setup 2

--- setup.py
+++ setup.py
@@ -13,9 +13,8 @@ setup(
     packages=find_packages(),
     install_requires=required,
     include_package_data=True,
-    package_data={
-        
-        'prompts': ['classification/prompts/*'],  # Include all files in a specific folder
-    },
+    data_files=[
+        ('classification/prompts', ['classification/prompts/*']),
+    ],
     scripts=['bin/bb_import', 'bin/bb_mover', 'bin/bb_archive']
 )
